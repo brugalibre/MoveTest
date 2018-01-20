25aa0366fb847b9f376a201c1b1ffd4fbfea2bd5
Dominic <Dominic@Piranha.fritz.box>
2018-01-20 14:20:09 +0100

    Initial commit

---
+++ MoveTest/test/com/myownb3/piranha/TestMove.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.myownb3.piranha;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Dominic
+ *
+ */
+class TestMove {
+
+    @Test
+    void test() {
+	fail("Not yet implemented");
+    }
+}
