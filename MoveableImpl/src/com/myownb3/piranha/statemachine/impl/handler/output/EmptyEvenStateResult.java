7fe5779469a393fb3cf4f7e6cc1b712b8b585cf3
Dominic <Dominic@Piranha>
2020-01-26 09:28:21 +0100

    r remove unused emptyeventstateresult

--- MoveableImpl/src/com/myownb3/piranha/statemachine/impl/handler/output/EmptyEvenStateResult.java
+++
@@ -1,7 +0,0 @@
-package com.myownb3.piranha.statemachine.impl.handler.output;
-
-import com.myownb3.piranha.statemachine.handler.output.EvenStateResult;
-
-public class EmptyEvenStateResult implements EvenStateResult {
-// Empty
-}
