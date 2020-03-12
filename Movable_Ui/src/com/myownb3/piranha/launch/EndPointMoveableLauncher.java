64086eadb94c672ead3a0ecbf6133e582d3a78c0
DStalder <DStalder@ADC-NB20141.intern.cube.ch>
2020-03-12 12:35:17 +0100

    R clean up EndPointMoveableLauncher

--- Movable_Ui/src/com/myownb3/piranha/launch/EndPointMoveableLauncher.java
+++ Movable_Ui/src/com/myownb3/piranha/launch/EndPointMoveableLauncher.java
@@ -42,51 +42,21 @@ import com.myownb3.piranha.ui.render.impl.PositionListPainter;
  */
 public class EndPointMoveableLauncher {
     private static int padding = 30;
-    
+
     public static void main(String[] args) throws InterruptedException {
 
-//	Position endPos = Positions.of(400 + padding, 10 + padding);
-//	Position endPos2 = Positions.of(400 + padding, 400 + padding);
-	Position endPos3 = Positions.of(10 + padding, 10 + padding);
-//	Position endPos4 = Positions.of(10 + padding, 400 + padding);
+	int height = 4;
+	int width = 4;
 	DefaultGrid grid = MirrorGridBuilder.builder()//
 		.withMaxX(510)//
 		.withMaxY(510)//
 		.withMinX(padding)//
 		.withMinY(padding)//
 		.build();
-//	GridElement endPosMarker = new SimpleGridElement(grid, endPos);
-//	GridElement endPosMarker2 = new SimpleGridElement(grid, endPos2);
-	GridElement endPosMarker3 = new SimpleGridElement(grid, endPos3);
-//	GridElement endPosMarker4 = new SimpleGridElement(grid, endPos4);
-	int height = 4;
-	int width = 4;
-
-	List<EndPointMoveable> moveables = new ArrayList<>(4);
-//	EndPointMoveable moveable = getMoveable(endPos, grid, 200, 200);
-//	EndPointMoveable moveable2 = getMoveable(endPos2, grid, 200, 200);
-	EndPointMoveable moveable3 = getMoveable(endPos3, grid, 200, 200);
-//	EndPointMoveable moveable4 = getMoveable(endPos4, grid, 200, 200);
-//	moveables.add(moveable);
-//	moveables.add(moveable2);
-	moveables.add(moveable3);
-//	moveables.add(moveable4);
-	
-	List<GridElement> gridElements = getAllGridElements(grid, height, width);
-//	gridElements.add(endPosMarker);
-//	gridElements.add(endPosMarker2);
-	gridElements.add(endPosMarker3);
-//	gridElements.add(endPosMarker4);
-//	GridElementPainter moveablePainter = new GridElementPainter(moveable, getColor(moveable), height, width);
-//	GridElementPainter moveablePainter2 = new GridElementPainter(moveable2, getColor(moveable2), height, width);
-	GridElementPainter moveablePainter3 = new GridElementPainter(moveable3, getColor(moveable3), height, width);
-//	GridElementPainter moveablePainter4 = new GridElementPainter(moveable4, getColor(moveable4), height, width);
-
-	List<Renderer> renderers = getRenderers(grid, height, width, gridElements);
-//	renderers.add(moveablePainter);
-//	renderers.add(moveablePainter2);
-	renderers.add(moveablePainter3);
-//	renderers.add(moveablePainter4);
+	List<Position> endPositions = getEndPositions();
+	List<GridElement> gridElements = getAllGridElements(grid, height, width, endPositions);
+	List<EndPointMoveable> moveables = getMoveables(grid, endPositions);
+	List<Renderer> renderers = getRenderers(grid, height, width, gridElements, moveables);
 
 	MainWindow mainWindow = new MainWindow(grid.getDimension().getWidth(), grid.getDimension().getHeight(), padding,
 		height);
@@ -96,13 +66,26 @@ public class EndPointMoveableLauncher {
 	preparePositionListPainter(renderers, positions);
     }
 
+    private static List<EndPointMoveable> getMoveables(DefaultGrid grid, List<Position> endPositions) {
+	return endPositions.stream()//
+		.map(endPos -> getMoveable(endPos, grid, 200, 200))//
+		.collect(Collectors.toList());
+    }
+
+    private static List<Position> getEndPositions() {
+	List<Position> endPositions = new ArrayList<>();
+	endPositions.add(Positions.of(400 + padding, 10 + padding));
+	endPositions.add(Positions.of(400 + padding, 400 + padding));
+	endPositions.add(Positions.of(10 + padding, 10 + padding));
+	endPositions.add(Positions.of(10 + padding, 400 + padding));
+	return endPositions;
+    }
+
     private static void preparePositionListPainter(List<Renderer> renderers, List<Position> positions) {
-	PositionListPainter renderer = renderers.stream()//
+	renderers.stream()//
 		.filter(PositionListPainter.class::isInstance)//
 		.map(PositionListPainter.class::cast)//
-		.findFirst()//
-		.get();
-	renderer.setPositions(positions);
+		.forEach(renderer -> renderer.setPositions(positions));
     }
 
     private static void showGuiAndStartPainter(MainWindow mainWindow) {
@@ -118,36 +101,40 @@ public class EndPointMoveableLauncher {
 	}).start();
     }
 
-    private static List<GridElement> getAllGridElements(DefaultGrid grid, int height, int width) {
+    private static List<GridElement> getAllGridElements(DefaultGrid grid, int height, int width, List<Position> endPositions) {
 
 	List<GridElement> allGridElement = new ArrayList<>();
 
 	int angle = 45;
 	int signum = calcSignum();
-	Position randomPosition = Positions.of(300, 110);
-	Position randomPosition2 = Positions.of(300, 300);
-	Position randomPosition3 = Positions.of(110, 110);
-	Position randomPosition4 = Positions.of(110, 300);
-//	MoveableObstacleImpl obstacle = new MoveableObstacleImpl(grid, randomPosition);
-//	MoveableObstacleImpl obstacle2 = new MoveableObstacleImpl(grid, randomPosition2);
+	Position randomPosition = Positions.of(300 + padding, 110 + padding);
+	Position randomPosition2 = Positions.of(300 + padding, 300 + padding);
+	Position randomPosition3 = Positions.of(110 + padding, 110 + padding);
+	Position randomPosition4 = Positions.of(110 + padding, 300 + padding);
+	MoveableObstacleImpl obstacle = new MoveableObstacleImpl(grid, randomPosition);
+	MoveableObstacleImpl obstacle2 = new MoveableObstacleImpl(grid, randomPosition2);
 	MoveableObstacleImpl obstacle3 = new MoveableObstacleImpl(grid, randomPosition3);
-//	MoveableObstacleImpl obstacle4 = new MoveableObstacleImpl(grid, randomPosition4);
+	MoveableObstacleImpl obstacle4 = new MoveableObstacleImpl(grid, randomPosition4);
 	if (signum < 0) {
-//	    obstacle.makeTurn(angle);
-//	    obstacle2.makeTurn(angle);
+	    obstacle.makeTurn(angle);
+	    obstacle2.makeTurn(angle);
 	    obstacle3.makeTurn(angle);
-//	    obstacle4.makeTurn(angle);
+	    obstacle4.makeTurn(angle);
 	} else {
-//	    obstacle.makeTurn(angle - 180);
-//	    obstacle2.makeTurn(angle - 180);
+	    obstacle.makeTurn(angle - 180);
+	    obstacle2.makeTurn(angle - 180);
 	    obstacle3.makeTurn(angle - 180);
-//	    obstacle4.makeTurn(angle - 180);
+	    obstacle4.makeTurn(angle - 180);
 	}
 
-//	allGridElement.add(obstacle);
-//	allGridElement.add(obstacle2);
+	allGridElement.add(obstacle);
+	allGridElement.add(obstacle2);
 	allGridElement.add(obstacle3);
-//	allGridElement.add(obstacle4);
+	allGridElement.add(obstacle4);
+	
+	for (Position endPos : endPositions) {
+	    allGridElement.add(new SimpleGridElement(grid, endPos));
+	}
 	return allGridElement;
     }
 
@@ -195,11 +182,15 @@ public class EndPointMoveableLauncher {
     }
 
     private static List<Renderer> getRenderers(DefaultGrid grid, int height, int width,
-	    List<GridElement> gridElements) {
+	    List<GridElement> gridElements, List<EndPointMoveable> moveables) {
 	List<Renderer> renderers = gridElements.stream()//
 		.map(gridElement -> new GridElementPainter(gridElement, getColor(gridElement), height, width))//
 		.collect(Collectors.toList());
 	renderers.add(new PositionListPainter(Collections.emptyList(), Color.GREEN, height, width));
+	
+	for (EndPointMoveable endPointMoveable : moveables) {
+	    renderers.add(new GridElementPainter(endPointMoveable, getColor(endPointMoveable), height, width));
+	}
 	return renderers;
     }
 
@@ -212,11 +203,9 @@ public class EndPointMoveableLauncher {
 	Position pos = Positions.of(posX + padding, posY + padding);
 	Detector detector = new DetectorImpl(config.getDetectorReach(), config.getDetectorAngle(),
 		config.getEvasionAngle(), config.getEvasionAngleInc());
-	EndPointMoveable moveable = EndPointMoveableBuilder.builder()
-		.withEndPosition(endPos)//
+	EndPointMoveable moveable = EndPointMoveableBuilder.builder().withEndPosition(endPos)//
 		.withGrid(grid)//
-		.withStartPosition(pos)
-		.withHandler(new EvasionStateMachine(detector, endPos, config))//
+		.withStartPosition(pos).withHandler(new EvasionStateMachine(detector, endPos, config))//
 		.widthEndPosition(endPos)//
 		.build();
 	moveable.prepare();
