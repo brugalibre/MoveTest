4adbefa8ccf5f1cf2cc1a6a10f5b68daa46394e9
Dominic <Dominic@Galaxy-Book>
2020-04-24 18:44:00 +0200

    T tests for ObstacleImpl

---
+++ MoveableImpl/test/com/myownb3/piranha/grid/gridelement/ObstacleImplTest.java
@@ -0,0 +1,30 @@
+package com.myownb3.piranha.grid.gridelement;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.myownb3.piranha.grid.Grid;
+import com.myownb3.piranha.grid.gridelement.position.Position;
+import com.myownb3.piranha.grid.gridelement.shape.circle.CircleImpl;
+
+class ObstacleImplTest {
+
+   @Test
+   void testConstructor() {
+
+      // Given
+      Grid grid = Mockito.mock(Grid.class);
+      Position position = Mockito.mock(Position.class);
+      CircleImpl shape = Mockito.mock(CircleImpl.class);
+
+      // When
+      Obstacle obstacle = new ObstacleImpl(grid, position, shape);
+
+      // Then
+      assertThat(obstacle.getShape(), is(shape));
+   }
+
+}
