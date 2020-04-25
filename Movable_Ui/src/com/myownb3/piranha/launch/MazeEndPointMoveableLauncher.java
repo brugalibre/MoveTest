0892a4d53eccb70af55a31f62d7155fa4a550aa0
Dominic <Dominic@Piranha>
2020-04-26 00:43:05 +0200

    fix up maze

--- Movable_Ui/src/com/myownb3/piranha/launch/MazeEndPointMoveableLauncher.java
+++ Movable_Ui/src/com/myownb3/piranha/launch/MazeEndPointMoveableLauncher.java
@@ -10,6 +10,7 @@ import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,6 +25,7 @@ import com.myownb3.piranha.grid.DefaultGrid;
 import com.myownb3.piranha.grid.Grid;
 import com.myownb3.piranha.grid.MirrorGrid.MirrorGridBuilder;
 import com.myownb3.piranha.grid.gridelement.GridElement;
+import com.myownb3.piranha.grid.gridelement.Obstacle;
 import com.myownb3.piranha.grid.gridelement.ObstacleImpl;
 import com.myownb3.piranha.grid.gridelement.position.EndPosition;
 import com.myownb3.piranha.grid.gridelement.position.EndPositions;
@@ -58,23 +60,24 @@ public class MazeEndPointMoveableLauncher {
 
    public static void main(String[] args) throws InterruptedException {
 
-      int height = 10;
-      int width = 100;
       MazeEndPointMoveableLauncher launcher = new MazeEndPointMoveableLauncher();
-      launcher.launch(height, width);
+      launcher.launch();
    }
 
-   private void launch(int height, int width) throws InterruptedException {
+   private void launch() throws InterruptedException {
       DefaultGrid grid = buildGrid(new DefaultCollisionDetectionHandlerImpl());
       MainWindow mainWindow = new MainWindow(grid.getDimension().getWidth(), grid.getDimension().getHeight(), padding,
-            height);
+            10);
 
       List<MoveableController> moveableControllerList = new ArrayList<>();
       List<Renderer> renderers = new ArrayList<>();
       EndPosition endPosition = EndPositions.of(400 + padding, 400 + padding);
-      List<GridElement> gridElements = getAllGridElements(grid, height, width, endPosition);
+      List<GridElement> gridElements = getAllGridElements(grid, endPosition);
+      // new EvasionStateMachineConfigImpl(1, 10, 0.06, 0.7d, 35, 30, 90, 50, 2);
       EvasionStateMachineConfig config = DefaultConfig.INSTANCE.getDefaultEvasionStateMachineConfig();
-      MoveableController controller = buildMoveableController(grid, Positions.of(200 + padding, 200 + padding), singletonList(endPosition),
+      Position startPos = Positions.of(165 + padding, 155 + padding);
+      startPos.rotate(-45);
+      MoveableController controller = buildMoveableController(grid, startPos, singletonList(endPosition),
             getPostMoveFowardHandler(mainWindow, moveableControllerList, emptyList(), renderers), config);
       moveableControllerList.add(controller);
       renderers.addAll(getRenderers(grid, 4, 4, gridElements, controller.getMoveable(), config));
@@ -139,22 +142,66 @@ public class MazeEndPointMoveableLauncher {
       }).start();
    }
 
-   private static List<GridElement> getAllGridElements(DefaultGrid grid, int height, int width, Position endPos) {
+   private static List<GridElement> getAllGridElements(DefaultGrid grid, Position endPos) {
 
       List<GridElement> allGridElement = new ArrayList<>();
+      allGridElement.addAll(buildWall1(grid));
+      allGridElement.addAll(buildWall2(grid));
+      allGridElement.addAll(buildGridElements(grid));
 
-      Position center = Positions.of(280 + padding, 280 + padding);
-      center.rotate(-45);
+      return allGridElement;
+   }
+
+   private static List<GridElement> buildGridElements(DefaultGrid grid) {
+      Position startPos = Positions.of(215 + padding, 205 + padding);
+      Obstacle simpleGridElem = new ObstacleImpl(grid, startPos, buildCircle(4, startPos));
+      return Arrays.asList(simpleGridElem);
+   }
+
+   private static List<GridElement> buildWall2(DefaultGrid grid) {
+      int height = 10;
+      int width = 100;
+
+      Position center1 = Positions.of(280 + padding, 280 + padding);
+      center1.rotate(-45);
+      GridElement rectangleGridElem = buildRectangleObstacle(grid, height, width, center1);
+
+      Position center2 = Positions.of(288 + padding - (width / 2), 180 + padding - (height / 2));
+      center2.rotate(-135);
+      GridElement rectangleGridElem2 = buildRectangleObstacle(grid, height, width * 2, center2);
+      return Arrays.asList(rectangleGridElem, rectangleGridElem2);
+   }
+
+   private static List<GridElement> buildWall1(DefaultGrid grid) {
+      int height = 10;
+      int width = 100;
+      Position center1 = Positions.of(295 + padding, 370 + padding);
+      center1.rotate(-45);
+      GridElement rectangleGridElem1 = buildRectangleObstacle(grid, height, 70, center1);
+
+      Position center2 = Positions.of(267 + padding - (width / 2), 342 + padding - (height / 2));
+      center2.rotate(-135);
+      GridElement rectangleGridElem2 = buildRectangleObstacle(grid, height, 80 * 2, center2);
+
+      Position center3 = Positions.of(200 + padding - (width / 2), 197 + padding - (height / 2));
+      center3.rotate(-135);
+      GridElement rectangleGridElem3 = buildRectangleObstacle(grid, height, 70 * 2, center3);
+
+      Position center4 = Positions.of(180 + padding, 260 + padding);
+      center4.rotate(-45);
+      GridElement rectangleGridElem4 = buildRectangleObstacle(grid, height, 65, center4);
+      return Arrays.asList(rectangleGridElem1, rectangleGridElem2, rectangleGridElem3, rectangleGridElem4);
+   }
+
+   private static GridElement buildRectangleObstacle(DefaultGrid grid, int height, int width, Position center) {
       Rectangle rectangle = new RectangleBuilder()
             .withCenter(center)
             .withHeight(height)
             .withWidth(width)
             .withOrientation(Orientation.HORIZONTAL)
             .build();
-
-      GridElement gridElement = new ObstacleImpl(grid, center, rectangle);
-      allGridElement.add(gridElement);
-      return allGridElement;
+      GridElement rectangleGridElem = new ObstacleImpl(grid, center, rectangle);
+      return rectangleGridElem;
    }
 
    private static List<Renderer> getRenderers(DefaultGrid grid, int height, int width,
@@ -163,7 +210,7 @@ public class MazeEndPointMoveableLauncher {
             .map(gridElement -> new GridElementPainter(gridElement, getColor(gridElement), height, width))
             .collect(Collectors.toList());
       renderers.add(new PositionListPainter(Collections.emptyList(), getPositionListColor(), height, width));
-      MoveablePainterConfig moveablePainterConfig = MoveablePainterConfig.of(config, true, false);
+      MoveablePainterConfig moveablePainterConfig = MoveablePainterConfig.of(config, false, false);
       renderers.add(new MoveablePainter(moveable, getColor(moveable), height, width, moveablePainterConfig));
       return renderers;
    }
